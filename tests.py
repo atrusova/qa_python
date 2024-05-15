622e1747468ae816ccde268c6dd844c51f2d2561
Anastasiia Trusova <trusovaanastasiaa@gmail.com>
2024-05-15 16:23:36 +0200

    Sprint 4

--- tests.py
+++ tests.py
@@ -1,24 +1,80 @@
+import pytest
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
+
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
+    def test_add_new_book_short_name_book_added_without_genre(self, book, short_book_name):
+        book.add_new_book(short_book_name)
+        assert short_book_name in book.books_genre and len(book.books_genre.values()) <= 1
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+    @pytest.mark.parametrize('name',
+        [
+            'Властелин колец: Братство Кольца (англ. The Lord of the Rings: The Fellowship of the Ring)',
+            'Властелин колец: 2 крепости (англ. The Lord of the Rings: The Two Towers)'
+        ]
+    )
+    def test_add_new_book_when_len_more_than40_not_in_list(self, book, name):
+        book.add_new_book(name)
+        assert name not in book.books_genre
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+    def test_set_book_genre_name_and_genre_exist_genre_set(self, book, short_book_name):
+        book.add_new_book(short_book_name)
+        book.set_book_genre(short_book_name, 'Фантастика')
+        assert book.books_genre.get('Властелин колец The Lord of the Rings RU')
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    @pytest.mark.parametrize(
+        'name, genre',
+        [
+            ['Властелин колец The Lord of the Rings RU', 'Фантастика'],
+            ['Озарк', 'Детективы'],
+            ['The Last of Us', 'Ужасы']
+        ]
+    )
+    def test_get_book_genre_exist_name_get_genre(self, book, name, genre, few_books_genre):
+        book.books_genre = few_books_genre
+        assert book.get_book_genre(name) == genre
+
+    @pytest.mark.parametrize(
+        'name, genre',
+        [
+            ['Властелин колец The Lord of the Rings RU', 'Фантастика'],
+            ['Озарк', 'Детективы'],
+            ['The Last of Us', 'Ужасы'],
+            ['Рик и Морти', 'Мультфильмы'],
+            ['Мальчишник в Вегасе', 'Комедии']
+        ],
+        ids=[
+            'Test for genre 1',
+            'Test for genre 2',
+            'Test for genre 3',
+            'Test for genre 4',
+            'Test for genre 5'
+            ]
+    )
+    def test_get_books_with_specific_genre_exist_genre_get_book_name(self, book, books_for_all_genre, name, genre):
+        book.books_genre = books_for_all_genre
+        assert book.get_books_with_specific_genre(genre) == [name]
+
+    def test_get_books_genre_few_books_get_genre(self, book, few_books_genre):
+        book.books_genre = few_books_genre
+        assert book.get_books_genre() == few_books_genre
+
+    def test_get_books_for_children_add_different_genre_get_correct_book(self, book, few_books_genre):
+        book.books_genre = few_books_genre
+        assert 'The Last of Us' not in book.get_books_for_children()
+
+    def test_add_book_in_favorites_send_exist_book_get_book(self, book, short_book_name):
+        book.add_new_book(short_book_name)
+        book.add_book_in_favorites(short_book_name)
+        assert book.favorites == ['Властелин колец The Lord of the Rings RU']
+
+    def test_delete_book_from_favorites_delete_exist_book_get_two_books(self, book, few_books):
+        book.favorites = few_books
+        book.delete_book_from_favorites('Озарк')
+        assert book.favorites == ['Властелин колец The Lord of the Rings RU', 'The Last of Us']
+
+    def test_get_list_of_favorites_books_add_favorites_books_get_list(self, book, short_book_name):
+        book.add_new_book(short_book_name)
+        book.add_book_in_favorites(short_book_name)
+        assert book.favorites == [short_book_name]
